05955a4705b5b174de2d725291a558160c848972
losting <moe.losting@gmail.com>
2023-05-16 17:36:04 +0800

    build: update dist

--- dist/ls.es.js
+++ dist/ls.es.js
@@ -2,24 +2,30 @@ const l = (r) => {
   if (typeof r != "string" || !r)
     throw new TypeError("Input must be a string and cannot be empty!");
   const n = Math.floor(Math.random() * 256), t = r.length, e = new Uint16Array(t + 1);
-  for (let a = 0; a <= t; a++)
-    e[a] = r.charCodeAt(a) ^ n;
+  for (let o = 0; o <= t; o++)
+    e[o] = r.charCodeAt(o) ^ n;
   e[t] = n;
-  const o = new Uint8Array(e.buffer), s = String.fromCharCode(...o);
-  return btoa(s);
+  const s = new Uint8Array(e.buffer), a = String.fromCharCode(...s);
+  return btoa(a);
 }, f = (r) => {
   if (typeof r != "string" || !r)
     throw new TypeError("Input must be a string and cannot be empty!");
   const n = atob(r), t = new Uint8Array(n.length);
-  for (let a = 0; a < t.length; a++)
-    t[a] = n.charCodeAt(a);
-  const e = new Uint16Array(t.buffer), o = e.length - 1, s = new Array(o);
-  for (let a = 0; a < o; a++)
-    s[a] = String.fromCharCode(e[a] ^ e[o]);
-  return s.join("");
+  for (let o = 0; o < t.length; o++)
+    t[o] = n.charCodeAt(o);
+  const e = new Uint16Array(t.buffer), s = e.length - 1, a = new Array(s);
+  for (let o = 0; o < s; o++)
+    a[o] = String.fromCharCode(e[o] ^ e[s]);
+  return a.join("");
 };
 let c = localStorage.getItem("__LS_PREFIX__") || "MOE_";
-const g = () => void localStorage.clear(), i = (r) => void localStorage.removeItem(`${c}${r}`), u = (r, n = !1) => {
+const g = () => void localStorage.clear(), i = (r) => {
+  if (!r)
+    throw new Error("Missing key!");
+  localStorage.removeItem(`${c}${r}`);
+}, u = (r, n = !1) => {
+  if (!r)
+    throw new Error("Missing key!");
   const t = localStorage.getItem(`${c}${r}`);
   if (!t)
     return;
@@ -31,9 +37,9 @@ const g = () => void localStorage.clear(), i = (r) => void localStorage.removeIt
   return e.encrypt && (e.value === "" || e.value === null || e.value === void 0 || typeof e.value == "string" && (e.value = f(e.value)), e.__isJson && (e.value = JSON.parse(f(e.value)))), n ? e : e.value;
 }, d = (r, n, t) => {
   if (!r)
-    throw new Error("缺少必要参数!");
+    throw new Error("Missing key!");
   let e;
-  const s = {
+  const a = {
     value: (() => {
       if (n === "" || n === null || n === void 0)
         return n;
@@ -49,16 +55,16 @@ const g = () => void localStorage.clear(), i = (r) => void localStorage.removeIt
     encrypt: t == null ? void 0 : t.encrypt,
     __isJson: e
   };
-  localStorage.setItem(`${c}${r}`, JSON.stringify(s));
+  localStorage.setItem(`${c}${r}`, JSON.stringify(a));
 }, y = (r) => {
   if (r === c)
     return;
   const n = [];
   for (let t = 0; t < localStorage.length; t++) {
-    const e = localStorage.key(t), o = localStorage.getItem(e);
+    const e = localStorage.key(t), s = localStorage.getItem(e);
     if (e.startsWith(c)) {
-      const s = e.replace(new RegExp(`^${c}`), r);
-      localStorage.setItem(s, o), n.push(e);
+      const a = e.replace(new RegExp(`^${c}`), r);
+      localStorage.setItem(a, s), n.push(e);
     }
   }
   c = r, localStorage.setItem("__LS_PREFIX__", r), n.forEach((t) => {
